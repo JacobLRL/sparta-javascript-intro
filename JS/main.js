6a7698f5faeb43cce2a989b076b17525cf9d8495
JacobLRL <JLittle@spartaglobal.com>
2019-01-11 15:07:27 +0000

    added more info into js file

--- JS/main.js
+++ JS/main.js
@@ -1,18 +1,47 @@
-//alert ("hello me!");
-console.log("hi in console");
-var myNumber = 75;
-var newNumber = 85;
-console.log(myNumber + " " + newNumber);
-console.log(myNumber + newNumber);
-console.log(Math.max(myNumber, newNumber));
+console.log("working");
 
-if (myNumber === newNumber) {
-    console.log("equal");
-} else if (myNumber > newNumber) {
-    console.log(myNumber);
-} else {
-    console.log(newNumber);
+// Integer
+var int1 = 1;
+var int2 = 2;
+
+console.log(int1 + int2)
+
+// Float
+var float1 = 2.30;
+var float2 = 12.67
+
+console.log(float1 - float2);
+
+//Strings
+var str1 = "this is string 1";
+var str2 = "this is string 2";
+var str3 = "this is string 3";
+
+var conCat = str1 + " hello world";
+var interpolated = `String 3: ${str3}`
+
+console.log(interpolated);
+
+//Boolean
+var bool1 = true;
+var bool2 = false;
+
+// ===== FALSY =====
+// ""
+// 0
+// undefined
+// null
+// NaN
+
+//===== TRUTHY =====
+//Everything else
+
+
+var answer = prompt("What is your name");
+
+if(answer) {
+    console.log(`Welcome ${answer}`);
+}else {
+    console.log("please provide your name");
+    
 }
-var answer = myNumber+newNumber;
-
-console.log("number " + myNumber + " plus number " + newNumber + " equals " + answer);
