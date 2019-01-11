19592d6e8b28956724f0ab8777d790d5033e647c
JacobLRL <JLittle@spartaglobal.com>
2019-01-11 11:46:45 +0000

    first commit

---
+++ JS/main.js
@@ -0,0 +1,18 @@
+//alert ("hello me!");
+console.log("hi in console");
+var myNumber = 75;
+var newNumber = 85;
+console.log(myNumber + " " + newNumber);
+console.log(myNumber + newNumber);
+console.log(Math.max(myNumber, newNumber));
+
+if (myNumber === newNumber) {
+    console.log("equal");
+} else if (myNumber > newNumber) {
+    console.log(myNumber);
+} else {
+    console.log(newNumber);
+}
+var answer = myNumber+newNumber;
+
+console.log("number " + myNumber + " plus number " + newNumber + " equals " + answer);
